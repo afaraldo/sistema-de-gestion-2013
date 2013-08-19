ff92deaaba0b6c421930de3cabec2e8ccab65246
afaraldo <iceberg.04@gmail.com>
2013-08-19 16:15:38 -0400

    Trabajo completo

--- formulario/js/form.js
+++ formulario/js/form.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $("#formulario").attr("action","register_complete.html").attr("method","get");
+    $("#formulario").innerHTML="Hola Mundo";
 });
 $(function(){
 	$( "#fecha" ).datepicker();
