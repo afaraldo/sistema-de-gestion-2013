68e1c285a09afdf83dc2ab1887ab9ac2c2672643
afaraldo <iceberg.04@gmail.com>
2013-08-22 16:17:42 -0400

    reparado

--- login/config/initializers/secret_token.rb
+++ login/config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Login::Application.config.secret_key_base = '6e47eca9650cb231be661a3903cd8a3a2de5c52c2df7eb1bed2df1b2b09d3cfe54e135c8ad1485740fce93ee88ea56ee2a902b7c3c2476180f8b46789e38958c'
+Login::Application.config.secret_key_base = 'b448881e2c9b4f3ab8246e47dfa1e07250d09d956f6fbbeb469c877e4c32ef760eba3625fa1e28a9f47647367429bcc5ca5ae16eec6404132ffb7d3f83d9e992'
